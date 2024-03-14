c1fe6ca8150f27e8008eaa0caa3f1f821a18106b
igniteking <khanzaidan786@gmail.com>
2024-03-14 18:58:26 +0530

    users webhook

--- convex/clerk.ts
+++ convex/clerk.ts
@@ -1,18 +1,120 @@
-"use node";
+import { ConvexError, v } from "convex/values";
+import {
+  MutationCtx,
+  QueryCtx,
+  internalMutation,
+} from "./_generated/server";
 
-import type { WebhookEvent } from "@clerk/clerk-sdk-node";
-import { v } from "convex/values";
-import { Webhook } from "svix";
+export async function getUser(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string
+) {
+  const user = await ctx.db
+    .query("users")
+    .withIndex("by_tokenIdentifier", (q) =>
+      q.eq("tokenIdentifier", tokenIdentifier)
+    )
+    .first();
 
-import { internalAction } from "./_generated/server";
+  if (!user) {
+    throw new ConvexError("expected user to be defined");
+  }
 
-const webhookSecret = process.env.CLEARK_WEBHOOK_SECRET || ``;
+  return user;
+}
 
-export const fulfill = internalAction({
-  args: { headers: v.any(), payload: v.string() },
-  handler: async (ctx, args) => {
-    const wh = new Webhook(webhookSecret);
-    const payload = wh.verify(args.payload, args.headers) as WebhookEvent;
-    return payload;
+export const createUser = internalMutation({
+  args: { tokenIdentifier: v.string(), name: v.string(), image: v.string() },
+  async handler(ctx, args) {
+    await ctx.db.insert("users", {
+      tokenIdentifier: args.tokenIdentifier,
+      orgIds: [],
+      name: args.name,
+      image: args.image,
+    });
   },
 });
+
+export const updateUser = internalMutation({
+  args: { tokenIdentifier: v.string(), name: v.string(), image: v.string() },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", args.tokenIdentifier)
+      )
+      .first();
+
+    if (!user) {
+      throw new ConvexError("no user with this token found");
+    }
+
+    await ctx.db.patch(user._id, {
+      name: args.name,
+      image: args.image,
+    });
+  },
+});
+
+// export const addOrgIdToUser = internalMutation({
+//   args: { tokenIdentifier: v.string(), orgId: v.string(), role: roles },
+//   async handler(ctx, args) {
+//     const user = await getUser(ctx, args.tokenIdentifier);
+
+//     await ctx.db.patch(user._id, {
+//       orgIds: [...user.orgIds, { orgId: args.orgId, role: args.role }],
+//     });
+//   },
+// });
+
+// export const updateRoleInOrgForUser = internalMutation({
+//   args: { tokenIdentifier: v.string(), orgId: v.string(), role: roles },
+//   async handler(ctx, args) {
+//     const user = await getUser(ctx, args.tokenIdentifier);
+
+//     const org = user.orgIds.find((org) => org.orgId === args.orgId);
+
+//     if (!org) {
+//       throw new ConvexError(
+//         "expected an org on the user but was not found when updating"
+//       );
+//     }
+
+//     org.role = args.role;
+
+//     await ctx.db.patch(user._id, {
+//       orgIds: user.orgIds,
+//     });
+//   },
+// });
+
+// export const getUserProfile = query({
+//   args: { userId: v.id("users") },
+//   async handler(ctx, args) {
+//     const user = await ctx.db.get(args.userId);
+
+//     return {
+//       name: user?.name,
+//       image: user?.image,
+//     };
+//   },
+// });
+
+// export const getMe = query({
+//   args: {},
+//   async handler(ctx) {
+//     const identity = await ctx.auth.getUserIdentity();
+
+//     if (!identity) {
+//       return null;
+//     }
+
+//     const user = await getUser(ctx, identity.tokenIdentifier);
+
+//     if (!user) {
+//       return null;
+//     }
+
+//     return user;
+//   },
+// });
